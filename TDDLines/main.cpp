17247f089f3ee62d0ec27264013705f0e495beb2
Omar <ocataldo@carthage.edu>
2020-04-28 10:09:30 -0500

    Updated Negative Tests

--- TDDLines/main.cpp
+++ TDDLines/main.cpp
@@ -30,6 +30,11 @@ TEST_CASE("Line Test") {
     REQUIRE(p1.getYString() == point2.str());
     REQUIRE(p1.getXString() != point2.str());
 
+    Point p2(-.6, 2.5);
+    double d3 = -.6;
+    stringstream point3;
+    point3 << d3;
+    REQUIRE(p2.getXString() == point3.str());
 
     string test = "X:" + p1.getXString() + " , Y:" + p1.getYString();
     REQUIRE(test == p1.getCoordinates());
